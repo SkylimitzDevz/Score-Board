151084195ab73c4e5696e46cae6d4985429a8dcd
Ridma Pawan <ridma.mc@gmail.com>
2025-11-06 23:04:31 +0530

    Used an advance JS code - made a custom UI - linked buttons to account

---
+++ app.js
@@ -0,0 +1,34 @@
+let score1 = document.getElementById("team1-score")
+let score2 = document.getElementById("team2-score")
+
+let scoreA = 0
+let scoreB = 0
+
+
+
+function add(team, points) {
+    console.log("Team:", team, "points", points)
+
+    let pointCount = Number(points)
+
+    console.log("ponits: " +pointCount)
+
+     if (team === "A") {
+        scoreA += pointCount
+        score1.innerText = scoreA
+     }
+
+     if (team === "B") {
+        scoreB += pointCount
+        score2.innerText = scoreB
+     }
+}
+
+function reset() {
+    score1.innerText = 0
+    score2.innerText = 0
+    scoreA = 0
+    scoreB = 0
+}
+
+
